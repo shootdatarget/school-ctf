42e5d5434d76ca98bbd4db22b88cbd3eefbf6fa5
A.A.A <shootdatarget@users.noreply.github.com>
2018-04-02 18:31:02 +0800

    Formatting
    Formatting

--- application/views/login.php
+++ application/views/login.php
@@ -38,7 +38,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 		<br>
       <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
 	 
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Please Sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" name="user_email" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
